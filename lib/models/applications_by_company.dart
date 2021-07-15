611dc5d2f0ae92c01fcc90a6642e4088b91d56e9
judhanny <judithhankin@googlemail.com>
2021-07-15 20:10:08 +0100

    Adding basic functionality to fetch some sample company details "from the internet". Added ability to persist list of job applications to local store file in json format.

--- lib/models/applications_by_company.dart
+++ lib/models/applications_by_company.dart
@@ -1,21 +1,27 @@
 import 'package:job_seeker/models/company.dart';
 import 'job_application.dart';
 
-class ApplicationsByCompany{
-  Map<String,Company> companiesMap ={};
+class ApplicationsByCompany {
+  Map<String, Company> companiesMap = {};
 
   ApplicationsByCompany(this.companiesMap);
 
-  void addJobApplication(JobApplication application){
-      String companyName = application.companyName.toUpperCase();
-      if(! companiesMap.containsKey(companyName)){
-        companiesMap[companyName] = new Company(companyName, [application]);
+  void addJobApplication(JobApplication application) {
+    String companyName = application.companyName.toUpperCase();
+    if (!companiesMap.containsKey(companyName)) {
+      companiesMap[companyName] = new Company(companyName, [application]);
+    }
+    else {
+      Company? company = companiesMap[companyName];
+      if (company != null) {
+        company.addJobApplication(application);
       }
-      else{
-        Company? company = companiesMap[companyName];
-        if(company != null){
-          company.addJobApplication(application);
-        }
-      }
+    }
+  }
+
+  void addJobApplications(List<JobApplication> applications) {
+    applications.forEach((application) {
+      this.addJobApplication(application);
+    });
   }
 }
